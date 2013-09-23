3f02d53565409a829eee078fb212b922e946d703
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2013-09-23 22:28:33 +1000

    add sign in cookie (now upto 8.2.5)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -37,8 +37,9 @@ describe "Authentication" do
 
             it { should have_title(user.name) }
             it { should have_link('Profile',    href: user_path(user)) }
-            it { should have_link('Sign Out',   href: signout_path) }
-            it { should_not have_link('Sign In',   href: sigin_path) }
+            it { should have_link('Sign out',   href: signout_path) }
+            it { should_not have_link('Sign in',   href: signin_path) }
+
         end
     end
 end
