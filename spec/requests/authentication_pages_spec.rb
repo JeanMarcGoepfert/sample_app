04e315f2efab3d0d9ddd05146d0a621bf97973ba
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2013-10-01 23:07:57 +1000

    user index (9.3.2)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,6 +36,7 @@ describe "Authentication" do
             it { should have_link('Settings',       href: edit_user_path(user)) }
             it { should have_link('Sign out',       href: signout_path) }
             it { should_not have_link('Sign in',    href: signin_path) }
+            it { should have_link('Users',      href: users_path) }
 
             describe "followed by signout" do
                 before { click_link("Sign out") }
@@ -48,6 +49,24 @@ describe "Authentication" do
 
         describe "for non-signed-in users" do
             let(:user) { FactoryGirl.create(:user) }
+
+            describe "in the Users controller" do
+
+                describe "visiting the edit page" do
+                    before { visit edit_user_path(user) }
+                    it { should have_title('Sign in') }
+                end
+
+                describe "submittin to the update action" do
+                    before { patch user_path(user) }
+                    specify { expect(response).to redirect_to(signin_path) }
+                end
+
+                describe "visiting the user index" do
+                    before { visit users_path }
+                    it { should have_title('Sign in') }
+                end
+            end
 
             describe "when attempting to visit a protected page" do
                 before do
@@ -63,16 +82,6 @@ describe "Authentication" do
                         expect(page).to have_title('Edit user')
                     end
                 end
-            end
-
-            describe "visiting the edit page" do
-                before { visit edit_user_path(user) }
-                it { should have_title('Sign in') }
-            end
-
-            describe "submittin to the update action" do
-                before { patch user_path(user) }
-                specify { expect(response).to redirect_to(signin_path) }
             end
         end
 
