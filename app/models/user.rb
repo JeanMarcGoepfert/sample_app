53261ad9cad5b9fd4ba998fbf37de007d2e40449
Jean-Marc Goepfert <goepfertjeanmarc@gmail.com>
2013-10-15 06:54:05 +1100

    add user/relationship associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6 }
     has_many :microposts, dependent: :destroy
+    has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 
     before_create :create_remember_token
 
